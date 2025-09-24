33492c32ed2d46b28a97f44009be1a3bf1b92f59
hasanulmuttaqin <hasanul.muttaqin@ui.ac.id>
2025-09-24 16:15:57 +0700

    tutorial 4: CSS styling

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from main.views import show_main, create_news, show_news, show_xml, show_json, show_xml_by_id, show_json_by_id
-from main.views import register, login_user, logout_user
+from main.views import register, login_user, logout_user, edit_news, delete_news
 
 app_name = 'main'
-#f
 
 urlpatterns = [
     path('', show_main, name='show_main'),
@@ -16,4 +15,6 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('news/<uuid:id>/edit', edit_news, name='edit_news'),
+    path('news/<uuid:id>/delete', delete_news, name='delete_news'),
 ]
